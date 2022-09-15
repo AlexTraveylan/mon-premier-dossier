217c13a710c5c930fe4ae6645e4b6208b1959442
AlexTraveylan <108667536+AlexTraveylan@users.noreply.github.com>
2022-09-15 22:02:34 +0200

    Update variables.php

--- config/variables.php
+++ config/variables.php
@@ -2,12 +2,12 @@
 
 //En ligne
 
-// $dossier='http://parent-nelson-mandela.xyz/';
+// $dossier=;
 
-// $mysql='host=127.0.0.1:3306';
-// $dbname='u463218933_association';
-// $dbutilisateur='u463218933_timothee';
-// $dbmdp='0M7dUiIZzk8TvcNzmGk0';
+// $mysql=;
+// $dbname=;
+// $dbutilisateur=;
+// $dbmdp=;
 
 
 
